76fe37439ae461a8bbfaba8575a03a039a266bc9
iheb <iheb.bentemessek@esprit.tn>
2025-04-10 06:32:28 +0100

    Add Scheduler

--- src/main/java/com/carpem/parser/ParserApplication.java
+++ src/main/java/com/carpem/parser/ParserApplication.java
@@ -2,8 +2,11 @@ package com.carpem.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class ParserApplication {
 
     public static void main(String[] args) {
